cffccfb6f97871616272bd57532459174dd32e9a
naeemraj83 <mdnaeemhossain0311@gmail.com>
2024-02-21 16:14:12 +0600

    7thcommit

--- index.js
+++ index.js
@@ -35,25 +35,19 @@ const allBtn = document.getElementsByClassName('add-btn');
 let count = 0;
 
 
+
 for (const button of allBtn) {
    button.addEventListener('click', function(e){
   count = count + 1;
-  counts  = count - 8;
+//   counts  = count - 8;
   
 
-  setText( 'n-count',count);
-setText('m-count', counts);
+  setText('n-count',count);
+// setText('m-count',counts);
 
 
-// const sitName = e.target.parentNode.childNodes[3].innerText ;
+
 const sitName = e.target.parentNode.childNodes[1].innerText ;
-
-
-// console.log(e.target.parentNode.parentNode);
-// console.log(e.target.parentNode.childNodes[1].innerText );
-
-
-
 const dataAll=document.getElementById('data-all');
 
 const li = document.createElement('li');
@@ -66,6 +60,12 @@ p2.innerText= 'AcBus';
 const p3 = document.createElement('p');
 p3.innerText= '550';
 
+ const color = e.target.parentNode.childNodes[1].style.backgroundColor = '#90EE90';
+// if( conNumber + 1>4){
+//    alert('r na ')
+//    return;
+// }
+
 li.appendChild(p);
 li.appendChild(p2);
 li.appendChild(p3);
@@ -73,13 +73,24 @@ dataAll.appendChild(li);
 
 const totalAmmount = document.getElementById('t-price').innerText;
 const convert = parseInt(totalAmmount);
-// const taka = document.get
-// console.log(convert + parseInt(p3.innerText) )
-
 const sum = convert + parseInt(p3.innerText);
 document.getElementById('t-price').innerText = sum ;
 
+const totalAmmountS = document.getElementById('g-price').innerText;
+const convertGrand = parseInt(totalAmmountS);
+const sum1 = convertGrand + parseInt(p3.innerText);
+document.getElementById('g-price').innerText = sum1 ;
 
+// const minus1 = document.getElementById('m-count');
+// if( minus1 + 1 < 5){
+//    // alert('r na ');
+//    return;
+// }
+
+ const upperNumber = document.getElementById('m-count').innerText;
+ const conNumber =parseInt(upperNumber);
+ const minus = conNumber - 1 ;
+ document.getElementById('m-count').innerText = minus;
 
 
    });
@@ -90,39 +101,11 @@ function setText(id, value){
     document.getElementById(id).innerText = value;
 }
 
+function hide (){
+  
+}
 
 
 
 
-//     const seats = document.querySelectorAll(".add-btn");
-// const tickets = document.querySelectorAll(".ticket")
 
-// let totalPrice = 0;
-// let seatCount =0;
-// let remainSeat =8;
-
-
-
-
-// for (let i = 0; i < seats.length; i++) {
-//     const seat = seats[i];
-    
-//     seat.addEventListener("click", function () {
-        
-
-      
-//         totalPrices = + 550;
-
-        
-
-        
-//         setText("price-count", totalPrice);
-//         setText("grand-total", totalPrice);
-//         setText("seat-count", seatCount);
-//         setText("remain-seat", remainSeat);
-//     })
-// }
-
-// // function setText(id, value){
-// //     document.getElementById(id).innerText = value;
-// // }
