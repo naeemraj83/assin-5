5f42ffc1418d87a37eb4a8fce756fb61046bf420
naeemraj83 <mdnaeemhossain0311@gmail.com>
2024-02-21 17:02:10 +0600

    8th commit

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
 console.log('done');
 
-
-// function makeColor(){
-//     document.button.style.backgroundColor='green';
-// }
-
-// document.getElementById('btn-a1').addEventListener('click', function(){
-// const locationId = document.getElementById('location');
-// const locationId1 = document.getElementById('location1');
-// const locationId2 = document.getElementById('location2');
-// locationId.innerText = ' naeem ';
-// locationId1.innerText = ' naeem1 ';
-// locationId2.innerText = ' naeem2 ';
-// locationId.append
-
-
-// })
-
-// document.getElementById('btn-a2').addEventListener('click', function(){
-//     const locationId = document.getElementById('location');
-//     const locationId1 = document.getElementById('location1');
-//     const locationId2 = document.getElementById('location2');
-//     locationId.innerText = ' naeem3 ';
-//     locationId1.innerText = ' naeem4 ';
-//     locationId2.innerText = ' naeem5 ';
-    
-    
-//     })
-
-
-
 const allBtn = document.getElementsByClassName('add-btn');
-// console.log(buttons);
 let count = 0;
 
 
@@ -39,13 +8,8 @@ let count = 0;
 for (const button of allBtn) {
    button.addEventListener('click', function(e){
   count = count + 1;
-//   counts  = count - 8;
-  
 
   setText('n-count',count);
-// setText('m-count',counts);
-
-
 
 const sitName = e.target.parentNode.childNodes[1].innerText ;
 const dataAll=document.getElementById('data-all');
@@ -53,8 +17,6 @@ const dataAll=document.getElementById('data-all');
 const li = document.createElement('li');
 const p = document.createElement('p');
 p.innerText = sitName ;
-// p.innerText = sitNameS ;
-
 const p2 = document.createElement('p');
 p2.innerText= 'AcBus';
 const p3 = document.createElement('p');
@@ -101,8 +63,14 @@ function setText(id, value){
     document.getElementById(id).innerText = value;
 }
 
-function hide (){
-  
+function hide(){
+  const homeSec =document.getElementById('home');
+  // console.log(homeSec.classList);
+  homeSec.classList.add('hidden');
+
+  const hiddenSec =document.getElementById('model');
+  hiddenSec.classList.remove('hidden');
+
 }
 
 
